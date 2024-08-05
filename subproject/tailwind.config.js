cb58615ae3a64d9fcd1626d2a81b827bf407c325
young24n <won8560@gmail.com>
2024-08-05 15:58:42 +0900

    initial state

---
+++ subproject/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//여기서 커스텀 css 사용 및 수정 가능
+//예제에 ES6 관련 내용이 없다.. IE 호환성 때문인가?
