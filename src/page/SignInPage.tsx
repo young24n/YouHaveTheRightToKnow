7d6a80d398373d627e7ac2afe9d4fcbfaef96d3d
young24n <won8560@gmail.com>
2024-08-14 15:02:38 +0900

    signInPage Navbar 추가

--- src/page/SignInPage.tsx
+++ src/page/SignInPage.tsx
@@ -1,66 +1,74 @@
+import Navbar from "../components/Navbar";
+
 function SignInPage() {
   return (
-    <div className="flex min-h-full w-[26rem] flex-1 flex-col justify-center px-9 py-12 shadow-lg rounded-xl">
-      <div className="mx-auto w-full max-w-lg">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
-        </h2>
-      </div>
+    <>
+    <Navbar></Navbar>
+    
+    <div className="mt-8">
+      <div className="flex min-h-full w-[26rem] flex-1 flex-col justify-center px-9 py-12 shadow-lg rounded-xl m-auto">
+        <div className="mx-auto w-full max-w-lg">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
 
-      <div className="mt-10 mx-auto w-full max-w-lg">
-        <form action="#" method="POST" className="space-y-6">
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                autoComplete="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-center justify-between">
-              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                Password
+        <div className="mt-10 mx-auto w-full max-w-lg">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
               </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm leading-6"
+                />
+              </div>
             </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                required
-                autoComplete="current-password"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm leading-6"
-              />
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 text-sm leading-6"
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <button
-              type="button"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
+            <div>
+              <button
+                type="button"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
 
-        <p className="mt-10 text-center text-sm text-gray-500">
-          계정이 아직 없으신가요?
-          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-            회원가입 하기!
-          </a>
-        </p>
+          <p className="mt-10 text-center text-sm text-gray-500">
+            계정이 아직 없으신가요?
+            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              {" 회원가입 하기"}
+            </a>
+          </p>
+        </div>
       </div>
     </div>
+    </>
   );
 }
 
