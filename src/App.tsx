e045c040182a28f197b0630fc84d6d984523b995
young24n <won8560@gmail.com>
2024-08-20 13:24:29 +0900

    라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import SignInPage from './page/SignInPage'
+import Navbar from './components/Navbar'
+import SignUpPage from './page/SignUpPage'
 import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<SignInPage/>}/>
+      <Route path='/nav' element={<Navbar/>}/>
+      <Route path='/SignUp' element={<SignUpPage/>}/>
     </Routes>
   )
 }
