985e8f54dd339ffe81c68fe57174d7c500707407
young24n <won8560@gmail.com>
2024-08-21 17:01:50 +0900

    feedback text 디자인 추가, onChange 관련 이벤트 props 조건 추가

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -1,16 +1,18 @@
 interface propsType{
     dataType: string,
     placeholder: string,
-    feedback?: string,
+    eventHandler: React.ChangeEventHandler<HTMLInputElement> ,
+    feedback?: string,   
 }
 
 export default function InputComponents(props :propsType){
-    const {dataType, placeholder, feedback} = props
+    const {dataType, placeholder, feedback, eventHandler} = props
     return(
         <div className="m-auto max-w-xs w-full">
             <p className="block text-sm font-semibold leading-6 text-gray-900">{dataType}</p>
-            <input type="text" placeholder={placeholder} className="input input-bordered outline-indigo-600 w-full mt-2" />
-            {feedback && <p>{feedback}</p>}
+            <input type="text" placeholder={placeholder} className="input input-bordered outline-indigo-600 w-full mt-2" 
+            onChange={eventHandler}/>
+            {feedback && <p className="block text-xs font-semibold leading-6 text-red-600">{feedback}</p>}
             {/*feedback 글씨는 빨간 글씨로 feedback 상황 발생시 input bordered도 빨간걸로 추가 
             onChange 발생시 다시 일반 포커스 border로 -> 파란색 포커스가 좋을 듯(현재 회색)*/}
         </div>
