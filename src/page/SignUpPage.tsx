3e508a4d9f3f061c27b3d727228beceb6aa35e6a
young24n <won8560@gmail.com>
2024-08-22 16:08:45 +0900

    실시간 유효성 확인 기능 추가

--- src/page/SignUpPage.tsx
+++ src/page/SignUpPage.tsx
@@ -1,17 +1,14 @@
-import { MouseEvent, useState } from "react"
+import { useState } from "react"
 import InputComponents from "../components/InputComponent"
 import Navbar from "../components/Navbar"
 import ButtonComponents from "../components/ButtonComponents"
-function SignUpPage(){
-    //feedback inputs ()
-    const [feedbackForEmail, setFeedbackForEmail] = useState("해당 입력란을 필수로 입력해야합니다.")// "이메일 형식과 일치하지 않습니다!", "이미 존재하는 계정!"(Confirm 시도 시)
-    const [feedbackForVerifyCode, setFeedbackForVerifyCode] = useState("잘못된 인증번호입니다.") //"서버와 연결 실패 잠시 후 다시 시도해 주세요", ""
-    const [feedbackForPassword, setFeedbackForPassword] = useState("비밀번호는 특수문자, 숫자을 포함하여 8자 이상으로 구성해야합니다.") //"완벽합니다!"
-    const [feedbackForConfirmPassword, setFeedbackForConfirmPassword] = useState("비밀번호가 일치하지 않습니다.") //"완벽합니다!"
 
-    //todo: alert 디자인, 서버 피드백 필요
+function SignUpPage() {
+    const [feedbackForEmail, setFeedbackForEmail] = useState("")
+    const [feedbackForVerifyCode, setFeedbackForVerifyCode] = useState("")
+    const [feedbackForPassword, setFeedbackForPassword] = useState("")
+    const [feedbackForConfirmPassword, setFeedbackForConfirmPassword] = useState("")
 
-    //user infomation object
     const [userInfo, setUserInfo] = useState({
         email: "", 
         verifyCode: "",
@@ -19,29 +16,78 @@ function SignUpPage(){
         passwordCheck: "", 
     })
 
-    //Loding state
     const [checkLoding, setCheckLoding] = useState(false)
     const [confirmLoding, setConfirmLoading] = useState(false)
 
-    return(
+    function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const email = event.target.value
+        setUserInfo((prev) => ({ ...prev, email }))
+    }
+
+    //자판 배열이 한컴 입력기면 입력이 안됨
+    function handleVerifyCodeChange(event: React.ChangeEvent<HTMLInputElement>) {
+        let verifyCode = event.target.value
+        event.target.value = verifyCode.replace(/\D/g, '').slice(0, 6)
+        setUserInfo((prev) => ({ ...prev, verifyCode }))
+    }
+
+    function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const password = event.target.value
+
+        setUserInfo((prev) => {
+            const updatedUserInfo = { ...prev, password }
+
+            // 비밀번호 확인 로직을 여기에서 바로 실행
+            if (updatedUserInfo.passwordCheck && updatedUserInfo.passwordCheck !== password) {
+                setFeedbackForConfirmPassword("비밀번호가 일치하지 않습니다.")
+            } else {
+                setFeedbackForConfirmPassword("완벽합니다!") // 피드백 초기화
+            }
+
+            return updatedUserInfo
+        })
+
+        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+        if (!passwordRegex.test(password)) {
+            setFeedbackForPassword("비밀번호는 특수문자, 숫자를 포함하여 8자 이상으로 구성해야 합니다.")
+        } else {
+            setFeedbackForPassword("완벽합니다!") // 피드백 초기화
+        }
+    }
+
+    function handleConfirmPasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const confirmPassword = event.target.value
+        setUserInfo((prev) => {
+            const updatedUserInfo = { ...prev, passwordCheck: confirmPassword }
+
+            if (confirmPassword !== updatedUserInfo.password) {
+                setFeedbackForConfirmPassword("비밀번호가 일치하지 않습니다.")
+            } else {
+                setFeedbackForConfirmPassword("완벽합니다!") // 피드백 초기화
+            }
+
+            return updatedUserInfo
+        })
+    }
+
+    return (
         <>
-            <Navbar></Navbar>
-            <div className="flex flex-col justify-center space-y-10 m-auto w-[26rem] px-9 py-12 shadow-lg rounded-xl mt-8 ">
-                <InputComponents dataType="Email" placeholder="example@email.com" feedback={feedbackForEmail} eventHandler={()=>{}}/>
+            <Navbar />
+            <div className="flex flex-col justify-center space-y-10 m-auto w-[26rem] px-9 py-12 shadow-lg rounded-xl mt-8">
+                <InputComponents type="text" dataType="Email" placeholder="example@email.com" feedback={feedbackForEmail} eventHandler={handleEmailChange} />
                 <div className="">
-                    <InputComponents dataType="Verify Code" placeholder="123456" feedback={feedbackForVerifyCode} eventHandler={()=>{}}/>
+                    <InputComponents type="text" dataType="Verify Code" placeholder="" feedback={feedbackForVerifyCode} eventHandler={handleVerifyCodeChange} />
                     <button
                         type="button"
                         className="flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 m-3"
-                        onClick={()=>{}}
+                        onClick={() => {}}
                     >
                         {"Check"}
                     </button>
                 </div>
-                <InputComponents dataType="Password" placeholder="Password" feedback={feedbackForPassword} eventHandler={()=>{}}/>
-                <InputComponents dataType="Password Confirm" placeholder="Re-enter Password" feedback={feedbackForConfirmPassword} eventHandler={()=>{}}/>
-                <ButtonComponents buttonText="Confirm" eventHandler={()=>{console.log("hi")}}/>
-                {/*check 함수 필요 (공백, email rex, password rex, 잘못된 인증번호(이거는 서버 요청), 재입력 틀림 )*/}
+                <InputComponents type="password" dataType="Password" placeholder="Password" feedback={feedbackForPassword} eventHandler={handlePasswordChange} />
+                <InputComponents type="password" dataType="Password Confirm" placeholder="Re-enter Password" feedback={feedbackForConfirmPassword} eventHandler={handleConfirmPasswordChange} />
+                <ButtonComponents buttonText="Confirm" eventHandler={() => { console.log(userInfo) }} />
             </div>
         </>
     )
