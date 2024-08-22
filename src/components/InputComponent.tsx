8ba0b93575207c5bd34d85d53e045efd7f6f9d19
young24n <won8560@gmail.com>
2024-08-22 15:11:49 +0900

    feedback 종류에 따라 text color가 변경되도록 변경

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -7,12 +7,16 @@ interface propsType{
 
 export default function InputComponents(props :propsType){
     const {dataType, placeholder, feedback, eventHandler} = props
+
+    const feedbackClass = feedback === "완벽합니다!" ? "text-green-600" : "text-red-600";
+
     return(
         <div className="m-auto max-w-xs w-full">
             <p className="block text-sm font-semibold leading-6 text-gray-900">{dataType}</p>
             <input type="text" placeholder={placeholder} className="input input-bordered outline-indigo-600 w-full mt-2" 
-            onChange={eventHandler}/>
-            {feedback && <p className="block text-xs font-semibold leading-6 text-red-600">{feedback}</p>}
+            onChange={eventHandler}
+            />
+            {feedback && <p className={`block text-xs font-semibold leading-6 ${feedbackClass}`}>{feedback}</p>}
             {/*feedback 글씨는 빨간 글씨로 feedback 상황 발생시 input bordered도 빨간걸로 추가 
             onChange 발생시 다시 일반 포커스 border로 -> 파란색 포커스가 좋을 듯(현재 회색)*/}
         </div>
