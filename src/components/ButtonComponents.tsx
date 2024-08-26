ad4f90d102cfb83bf0d0aa3ef07147a819d755eb
young24n <won8560@gmail.com>
2024-08-26 16:39:32 +0900

    disable 제어 요소 추가, className 변수로 분리

--- src/components/ButtonComponents.tsx
+++ src/components/ButtonComponents.tsx
@@ -1,18 +1,28 @@
 import React from "react"
 
-interface propsType{
-    buttonText: string
-    eventHandler: React.MouseEventHandler<HTMLButtonElement>;
+interface propsType {
+    buttonText: string,
+    eventHandler: React.MouseEventHandler<HTMLButtonElement>,
+    disable?: boolean,
 }
 
-export default function ButtonComponents(props: propsType){
-    const {buttonText, eventHandler} = props
-    return(
+export default function ButtonComponents(props: propsType) {
+    const { buttonText, eventHandler, disable } = props
+
+    // 버튼 클래스 변수로 분리
+    const buttonClass = `
+        flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
+        ${disable ? 'bg-gray-400 cursor-auto' : 'bg-indigo-600 hover:bg-indigo-500 cursor-pointer'}
+        focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
+    `
+
+    return (
         <div>
             <button
                 type="button"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className={buttonClass}
                 onClick={eventHandler}
+                disabled={disable}
             >
                 {buttonText}
             </button>
