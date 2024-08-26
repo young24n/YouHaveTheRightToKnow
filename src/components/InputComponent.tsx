ae72c17c82a2e96ddf9ab70dd4fb5f82d67b2867
young24n <won8560@gmail.com>
2024-08-26 16:36:13 +0900

    feedback에 따른 input focus 디자인 적용

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -1,25 +1,30 @@
-interface propsType{
+import React from "react";
+
+interface PropsType {
     type: string,
     dataType: string,
     placeholder: string,
-    eventHandler: React.ChangeEventHandler<HTMLInputElement> ,
-    feedback?: string,   
+    eventHandler: React.ChangeEventHandler<HTMLInputElement>,
+    feedback?: string,
 }
 
-export default function InputComponents(props :propsType){
-    const {type, dataType, placeholder, feedback, eventHandler} = props
+export default function InputComponents(props: PropsType) {
+    const { type, dataType, placeholder, feedback, eventHandler } = props
 
     const feedbackClass = feedback === "완벽합니다!" ? "text-green-600" : "text-red-600";
+    const borderClass = feedback === "완벽합니다!" ? "border-green-600 input-success" : feedback ? "border-red-600 input-error" : "border-gray-300 input-primary"
+    const inputClass = `input w-full mt-2 ${borderClass}`
 
-    return(
+    return (
         <div className="m-auto max-w-xs w-full">
             <p className="block text-sm font-semibold leading-6 text-gray-900">{dataType}</p>
-            <input type={type} placeholder={placeholder} className="input input-bordered outline-indigo-600 w-full mt-2" 
-            onChange={eventHandler}
+            <input
+                type={type}
+                placeholder={placeholder}
+                className={inputClass}
+                onChange={eventHandler}
             />
             {feedback && <p className={`block text-xs font-semibold leading-6 ${feedbackClass}`}>{feedback}</p>}
-            {/*feedback 글씨는 빨간 글씨로 feedback 상황 발생시 input bordered도 빨간걸로 추가 
-            onChange 발생시 다시 일반 포커스 border로 -> 파란색 포커스가 좋을 듯(현재 회색)*/}
         </div>
-    )   
+    )
 }
