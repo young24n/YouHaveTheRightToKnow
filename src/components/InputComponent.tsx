2b1fd520694e5a4f359082356b3b56400fc7c389
young24n <won8560@gmail.com>
2024-08-28 09:47:43 +0900

    input component disable option add

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -6,10 +6,11 @@ interface PropsType {
     placeholder: string,
     eventHandler: React.ChangeEventHandler<HTMLInputElement>,
     feedback?: string,
+    disable?: boolean
 }
 
 export default function InputComponents(props: PropsType) {
-    const { type, dataType, placeholder, feedback, eventHandler } = props
+    const { type, dataType, placeholder, feedback, eventHandler, disable } = props
 
     const feedbackClass = feedback === "완벽합니다!" ? "text-green-600" : "text-red-600";
     const borderClass = feedback === "완벽합니다!" ? "border-green-600 input-success" : feedback ? "border-red-600 input-error" : "border-gray-300 input-primary"
@@ -23,6 +24,7 @@ export default function InputComponents(props: PropsType) {
                 placeholder={placeholder}
                 className={inputClass}
                 onChange={eventHandler}
+                disabled={disable}
             />
             {feedback && <p className={`block text-xs font-semibold leading-6 ${feedbackClass}`}>{feedback}</p>}
         </div>
