0e1db533588edc32265fccdd01a07aba723d2b84
young24n <won8560@gmail.com>
2024-08-29 15:06:59 +0900

    게시판 페이지 파일 생성

---
+++ src/page/PostPage.tsx
@@ -0,0 +1,9 @@
+function PostPage(){
+    // 포스트 갯수? 10~20?, Pagination 지원, 검색, 
+    // 처음부터 모든 게시글을 불러오는 것은 불가능하니 Pagination 숫자에 의해 요청하는 형식으로 (작성날짜(년/달/일)시간(시/분))
+    return(
+    <div>
+        
+    </div>
+    )
+}
