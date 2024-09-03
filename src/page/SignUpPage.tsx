68d2ceee84ab7837cc3d81156462bfd49d69f2fd
young24n <won8560@gmail.com>
2024-09-03 11:12:58 +0900

    SignUp: 메일 전송, 인증번호 검증, 관련 피드백 및 인증 후 예외처리 구현, Button의 disable 시각 효과 강화

--- src/page/SignUpPage.tsx
+++ src/page/SignUpPage.tsx
@@ -3,17 +3,18 @@ import InputComponents from "../components/InputComponent"
 import Navbar from "../components/Navbar"
 import ButtonComponents from "../components/ButtonComponents"
 import apiClient from "../apiClient"
-//todo: feedback 주는 곳으로 ref 설정
-//todo: 요청 전 네트워크 상태 확인 후 빠른 피드백 가능한지 확인
-//(이전 프로젝트에서 원인이 확실한 경우 timeout이 5초 동안 유지되는 것은 불쾌했음 timeout을 줄이기엔 불안정한 상황 또한 고려해야함)
-//과학적 입력(1e6)와 소수점 입력하는 경우를 막아야함
-
+import { Loading } from "react-daisyui"
+// todo: feedback 주는 곳으로 ref 설정
+// todo: 요청 전 네트워크 상태 확인 후 빠른 피드백 가능한지 확인
+// (이전 프로젝트에서 원인이 확실한 경우 timeout이 5초 동안 유지되는 것은 불쾌했음 timeout을 줄이기엔 불안정한 상황 또한 고려해야함)
+// response 타입 정의
 interface UserInfo{
     email: string, 
     nickName: string
     verifyCode: string,
     password: string, 
     passwordCheck: string, 
+    isVerify: boolean
 }
 
 function SignUpPage() {
@@ -29,45 +30,48 @@ function SignUpPage() {
         verifyCode: "",
         password: "", 
         passwordCheck: "", 
+        isVerify: false
     })
+    const [isChecked, setIsChecked] = useState<boolean>(false)
 
-    const [checkLoding, setCheckLoding] = useState<boolean>(false)
-    const [confirmLoding, setConfirmLoading] = useState<boolean>(false)
-    
+    const [checkLoading, setCheckLoading] = useState<boolean>(false)
+    const [confirmLoading, setConfirmLoading] = useState<boolean>(false)
+
+    const [verifyBtnText, setVerifyBtnText] = useState<string>("Send Code")
     //추후 기능 관련 함수는 파일을 분리하는 것이 좋아보임
-    function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+    function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>){
         const email = event.target.value
         setUserInfo((prev: UserInfo) => ({ ...prev, email }))
     }
 
-    function handleNickNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    function handleNickNameChange(event: React.ChangeEvent<HTMLInputElement>){
         const nickName = event.target.value
         setUserInfo((prev: UserInfo) => ({ ...prev, nickName }))
     }
 
     //CAUTION: 자판 배열이 한컴 입력기일 경우 입력이 안됨
-    function handleVerifyCodeChange(event: React.ChangeEvent<HTMLInputElement>) {
+    function handleVerifyCodeChange(event: React.ChangeEvent<HTMLInputElement>){
         const verifyCode = event.target.value
         event.target.value = verifyCode.replace(/\D/g, '').slice(0, 6)
         setUserInfo((prev: UserInfo) => ({ ...prev, verifyCode }))
     }
 
-    function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+    function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>){
         const password = event.target.value
         setUserInfo((prev: UserInfo) => ({ ...prev, password }))
 
         const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
-        if(!passwordRegex.test(password)) {
+        if(!passwordRegex.test(password)){
             setFeedbackForPassword("비밀번호는 특수문자, 숫자를 포함하여 8자 이상으로 구성해야 합니다.")
         } 
         else{
             setFeedbackForPassword("완벽합니다!")
         }
 
-        if(userInfo.passwordCheck && userInfo.passwordCheck !== password) {
+        if(userInfo.passwordCheck && userInfo.passwordCheck !== password){
             setFeedbackForConfirmPassword("비밀번호가 일치하지 않습니다.")
         } 
-        else if(userInfo.passwordCheck === password && userInfo.passwordCheck !== "") {
+        else if(userInfo.passwordCheck === password && userInfo.passwordCheck !== ""){
             setFeedbackForConfirmPassword("완벽합니다!")
         } 
         else{
@@ -75,48 +79,94 @@ function SignUpPage() {
         }
     }
 
-    function handleConfirmPasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+    function handleConfirmPasswordChange(event: React.ChangeEvent<HTMLInputElement>){
         const confirmPassword = event.target.value
         setUserInfo((prev: UserInfo) => ({ ...prev, passwordCheck: confirmPassword }))
 
-        if(confirmPassword === userInfo.password && confirmPassword !== "") {
+        if(confirmPassword === userInfo.password && confirmPassword !== ""){
             setFeedbackForConfirmPassword("완벽합니다!")
         } 
-        else if (confirmPassword !== userInfo.password && confirmPassword !== "") {
+        else if(confirmPassword !== userInfo.password && confirmPassword !== ""){
             setFeedbackForConfirmPassword("비밀번호가 일치하지 않습니다.")
         } 
-        else {
+        else{
             setFeedbackForConfirmPassword("")
         }
     }
 
-    function handleVerifyCodeCheck(event: React.MouseEvent<HTMLButtonElement>){
-        //todo: 서버 전송 후 response에 따른 제어 형식으로
-        const response = apiClient.Post('/api/emails/verification-requests',{
-            email: userInfo.email
-        }).then(()=>{console.log(response)})
-        // 인증번호 보내기 -> 확인 -> 로딩 -> 성공, 실패(인증실패, 네트워크문제) -> 피드백 -> 성공 시 위쪽 전부 disable (인증 후 내용 수정 방지)
-        // 고려할게 많긴하구만 분리해서 단계적으로 보여주는 이유를 알것 같아
-        // button text도 여기서 변경해야함 send -> loading... -> check -> completed(버튼과 이메일 비활성화)
-        // alert 말고 feedback을 그냥 이용하자 
+    async function handleVerifyCodeCheck(event: React.MouseEvent<HTMLButtonElement>){
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        const trimmedEmail = userInfo.email.trim()
+        if(!emailRegex.test(trimmedEmail)){
+            setFeedbackForEmail("이메일 형식과 일치하지 않습니다!")
+            return
+        }
+    
+        try{
+            setCheckLoading(true)  // 로딩 상태 시작
+            setVerifyBtnText("Loading...")
+    
+            const response = await apiClient.Post(`/api/emails/verification-requests?email=${encodeURIComponent(trimmedEmail)}`)                
+    
+            if(response.status === 200){
+                setFeedbackForVerifyCode("인증번호가 전송되었습니다.")
+                setVerifyBtnText("Check Code")
+            } 
+            else{
+                setFeedbackForVerifyCode("인증번호 전송에 실패했습니다.")
+                setVerifyBtnText("Send Code")
+            }
+        } 
+        catch(error){
+            setFeedbackForVerifyCode("네트워크 오류가 발생했습니다.")
+        } 
+        finally{
+            setCheckLoading(false) 
+            setFeedbackForEmail("") 
+        }
+    }
+    
+    async function handleVerifyCodeConfirm(event: React.MouseEvent<HTMLButtonElement>){
+        try{
+            if(userInfo.verifyCode === ""){
+                setFeedbackForVerifyCode("인증번호를 입력하세요")
+            }
+            else if(userInfo.verifyCode.length < 6){
+                setFeedbackForVerifyCode("인증번호는 6자리 모두 입력해야 합니다.")
+            }
+            else{
+                setCheckLoading(true)
+                const response = await apiClient.Get(`/api/emails/verification`,{
+                    params: {
+                        email: userInfo.email,
+                        code: userInfo.verifyCode,
+                    }
+                })
+    
+                if(response.status === 200){
+                    setFeedbackForVerifyCode("인증이 완료되었습니다!")
+                    setIsChecked(true)
+                }
+                else{
+                    setFeedbackForVerifyCode("인증에 실패했습니다. 다시 시도해 주세요")
+                    setVerifyBtnText("Send Code")
+                }
+            }
+            setCheckLoading(false)
+        }
+        catch(error){
 
-        // if(401 or 403) {
-        //     setFeedbackForVerifyCode("인증번호가 일치하지 않습니다.")
-        // } 
-        // else if(405){
-        //     setFeedbackForVerifyCode("서버와 연결 어쩌구")
-        // }
-        // else if(네트워크 에러)
-        // else if(200){전송완료!}
-        // else {예상치 못한 에러}
+        }
+        //이메일 일치 하지 않음 -> 서버 검증에 따른 피드백 하기
+
     }
 
-    function handleConfirmCheck(event: React.MouseEvent<HTMLButtonElement>) {
+    function handleConfirmCheck(event: React.MouseEvent<HTMLButtonElement>){
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
         const nickNameRegex = /^[가-힣a-zA-Z]{2,10}$/
     
         // 이메일 검증
-        if(!emailRegex.test(userInfo.email)) {
+        if(!emailRegex.test(userInfo.email)){
             setFeedbackForEmail("이메일 형식과 일치하지 않습니다!")
         } 
         else{
@@ -125,20 +175,27 @@ function SignUpPage() {
         }    
     
         // 닉네임 검증
-        if(userInfo.nickName.length < 2) {
+        if(userInfo.nickName.length < 2){
             setFeedbackForNickName("닉네임은 최소 2자 이상이어야 합니다.")
         } 
-        else if(userInfo.nickName.length > 10) {
+        else if(userInfo.nickName.length > 10){
             setFeedbackForNickName("닉네임은 최대 10자까지만 입력할 수 있습니다.")
         } 
-        else if(!nickNameRegex.test(userInfo.nickName)) {
+        else if(!nickNameRegex.test(userInfo.nickName)){
             setFeedbackForNickName("닉네임은 한글과 영문만 사용할 수 있습니다.")
         } 
+        else if(!userInfo.isVerify){
+            setFeedbackForVerifyCode("이메일 인증을 먼저 완료하여야 합니다.")
+        }
         else{
             // 서버 요청,피드백
-            
-            // const {data, loading, error} = useRequest(()=>{})
+            const response = apiClient.Post(`/api/auths/join
+                ?email=${encodeURIComponent(userInfo.email)}
+                &password=${encodeURIComponent(userInfo.password)}
+                &nickname=${encodeURIComponent(userInfo.nickName)}`)
+
             setFeedbackForNickName("")
+            setFeedbackForVerifyCode("")
         }
     }
 
@@ -146,16 +203,20 @@ function SignUpPage() {
         <>
             <Navbar visibleSubmenu={false} logoRedirectUrl="/"/>
             <div className="flex flex-col justify-center space-y-7 m-auto w-[26rem] px-9 py-12 shadow-lg rounded-xl mt-8">
-                <InputComponents type="text" dataType="Email" placeholder="example@email.com" feedback={feedbackForEmail} eventHandler={handleEmailChange} />
+                <InputComponents type="text" dataType="Email" placeholder="example@email.com" feedback={feedbackForEmail} eventHandler={handleEmailChange} disable={isChecked} />
                 <div className="">
-                    <InputComponents type="text" dataType="Verify Code" placeholder="" feedback={feedbackForVerifyCode} eventHandler={handleVerifyCodeChange} />
+                    <InputComponents type="text" dataType="Verify Code" placeholder="" feedback={feedbackForVerifyCode} eventHandler={handleVerifyCodeChange} disable={isChecked} />
                     <button
                         type="button"
-                        className="flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 m-3"
-                        onClick={handleVerifyCodeCheck}
+                        className={`flex justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm m-3
+                        ${isChecked ? 'bg-gray-500 cursor-auto opacity-50' : 'bg-indigo-600 hover:bg-indigo-500 cursor-pointer'}
+                        focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+                        onClick={verifyBtnText === "Send Code" ? handleVerifyCodeCheck : handleVerifyCodeConfirm }
+                        disabled={isChecked}
                     >
-                        {"Send Code"}
+                        {verifyBtnText}
                     </button>
+
                 </div>
                 <InputComponents type="text" dataType="Nick Name" placeholder="" feedback={feedbackForNickName} eventHandler={handleNickNameChange} />
                 <InputComponents type="password" dataType="Password" placeholder="Password" feedback={feedbackForPassword} eventHandler={handlePasswordChange} />
