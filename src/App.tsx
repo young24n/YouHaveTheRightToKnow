ef065666fe9c93a5ae79a920a19f84293a8941bc
young24n <won8560@gmail.com>
2024-09-03 11:13:21 +0900

    route update

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import SignInPage from './page/SignInPage'
 import Navbar from './components/Navbar'
 import SignUpPage from './page/SignUpPage'
 import {Routes, Route} from 'react-router-dom'
+import InformationPage from './page/InformationPage'
+import PostPage from './page/PostPage'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<SignInPage/>}/>
-      <Route path='/nav' element={<Navbar/>}/>
+      <Route path='/information' element={<InformationPage/>}/>
+      <Route path='/post' element={<PostPage/>}/>
       <Route path='/SignUp' element={<SignUpPage/>}/>
     </Routes>
   )
