e8b20ab8396fe535c6d68c92f31e7874d268096d
young24n <won8560@gmail.com>
2024-09-05 12:15:58 +0900

    button components: text가 줄바꿈 하지 않도록 수정, 외부에서 class를 적용할 수 있도록 수정, 필요없는 div 삭제

--- src/components/ButtonComponents.tsx
+++ src/components/ButtonComponents.tsx
@@ -1,31 +1,27 @@
-import React from "react"
-
 interface propsType {
     buttonText: string,
     eventHandler: React.MouseEventHandler<HTMLButtonElement>,
     disable?: boolean,
+    className?: string,// className 추가
 }
 
 export default function ButtonComponents(props: propsType) {
-    const { buttonText, eventHandler, disable } = props
+    const { buttonText, eventHandler, disable, className } = props
 
-    // 버튼 클래스 변수로 분리
     const buttonClass = `
-        flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
-        ${disable ? 'bg-gray-400 cursor-auto' : 'bg-indigo-600 hover:bg-indigo-500 cursor-pointer'}
-        focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
-    `
+    flex items-center justify-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm whitespace-nowrap
+    ${disable ? 'bg-gray-400 cursor-auto' : 'bg-indigo-600 hover:bg-indigo-500 cursor-pointer'}
+    focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
+    ${className}`// 추가된 className을 적용
 
     return (
-        <div>
-            <button
-                type="button"
-                className={buttonClass}
-                onClick={eventHandler}
-                disabled={disable}
-            >
-                {buttonText}
-            </button>
-        </div>
+        <button
+            type="button"
+            className={buttonClass}
+            onClick={eventHandler}
+            disabled={disable}
+        >
+            {buttonText}
+        </button>
     )
 }
