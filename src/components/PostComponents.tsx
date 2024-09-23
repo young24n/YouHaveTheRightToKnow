40b62bd80011cf797714b8a3f8acfc8b536aa51d
young24n <won8560@gmail.com>
2024-09-23 09:40:37 +0900

    postComponents Detail 페이지로 접근하는 로직 구현

--- src/components/PostComponents.tsx
+++ src/components/PostComponents.tsx
@@ -1,15 +1,31 @@
-interface propsType{
-    title: string,
-    date: string,
-    time: string,
+// PostComponents.tsx
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+interface PropsType {
+    id: number
+    title: string
+    date: string
+    time: string
     count: number
 }
 
-export default function PostComponents(props: propsType){
-    const {date, title, time, count} = props
-    return(
+export default function PostComponents(props: PropsType) {
+    const { id, date, title, time, count } = props
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate(`/posts/${id}`)
+    }
+
+    return (
         <div className="flex flex-row justify-start p-3">
-            <a href="/post" className="w-80 whitespace-nowrap truncate max-w-lg">{title}</a>
+            <a
+                onClick={handleClick}
+                className="w-80 whitespace-nowrap truncate max-w-lg cursor-pointer"
+            >
+                {title}
+            </a>
             <p className="w-32 whitespace-nowrap">{date}</p>
             <p className="w-20 whitespace-nowrap">{time}</p>
             <p className="w-10 whitespace-nowrap">{count}</p>
