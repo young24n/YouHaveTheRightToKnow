525b21e76eb8cc36a497b1cceeab4142cbf86150
young24n <won8560@gmail.com>
2024-09-23 09:48:18 +0900

    pagination 로직 구현

--- src/components/PaginationConponent.tsx
+++ src/components/PaginationConponent.tsx
@@ -1,13 +1,38 @@
-import { Button, Pagination } from "react-daisyui";
+import { Button } from "react-daisyui"
 
-//하위 요소들을 조회하고 반복 시켜야함
-export default function PaginationComponent(){
-    return(
-        <div className="join">
-            <button className="join-item btn">1</button>
-            <button className="join-item btn btn-active">2</button>
-            <button className="join-item btn">3</button>
-            <button className="join-item btn">4</button>
-        </div>  
+interface propsType{
+    currentPage: any,
+    totalPages: any,
+    onPageChange: any
+}
+
+export default function PaginationComponent(props: propsType) {
+    const { currentPage, totalPages, onPageChange } = props
+
+    // 페이지 번호 배열 생성
+    const pageNumbers: number[] = []
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i)
+    }
+
+    // 페이지 버튼 클릭 핸들러
+    const handlePageClick = (pageNumber: number) => {
+        if (pageNumber !== currentPage) {
+            onPageChange(pageNumber)
+        }
+    }
+
+    return (
+        <div className="join w-full justify-center">
+            {pageNumbers.map((pageNumber) => (
+                <button
+                    key={pageNumber}
+                    className={`join-item btn ${pageNumber === currentPage ? 'btn-active' : ''}`}
+                    onClick={() => handlePageClick(pageNumber)}
+                >
+                    {pageNumber}
+                </button>
+            ))}
+        </div>
     )
-} 
+}
